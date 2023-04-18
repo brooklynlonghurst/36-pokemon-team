e31de4e01cc0917f893f19792a1be0cf7a050ab3
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-04-18 16:03:25 -0600

    implements useEffect hook

--- src/screens/DexScreen.jsx
+++ src/screens/DexScreen.jsx
@@ -1,9 +1,25 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import axios from "axios"
 
 function DexScreen() {
+    const [pokemon, setPokemon] = useState([])
+
+    useEffect(() => {
+        axios
+            .get(`https://pokeapi.co/api/v2/pokemon?limit=1000`)
+            .then((res) => {
+                setPokemon(res.data.results)
+            })
+    }, [])
+
+    const pokemonDisplay = pokemon.map((poke, index) => {
+        return <h4>{poke.name}</h4>
+    })
+
     return (
         <div>
-            <h1>My Dex</h1>
+            <h1>Pokedex</h1>
+            {pokemonDisplay}
         </div>
     )
 }
