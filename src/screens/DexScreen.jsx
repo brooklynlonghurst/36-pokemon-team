f67d33fe8924b902674915793127d36d08bdd632
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-04-18 10:11:06 -0600

    inital commit

---
+++ src/screens/DexScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function DexScreen() {
+    return (
+        <div>
+            <h1>My Dex</h1>
+        </div>
+    )
+}
+
+export default DexScreen
---
+++ src/screens/TeamScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function TeamScreen() {
+    return (
+        <div>
+            <h1>My Team</h1>
+        </div>
+    )
+}
+
+export default TeamScreen
