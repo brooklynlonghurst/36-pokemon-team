07bf9c0e5bfb58a387630b2c9f200e3eda784828
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-04-21 15:59:15 -0600

    works on css for stats

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -5,10 +5,14 @@ function TeamCard({pokemon}){
         <div className="card">
             <img src={pokemon.sprites.other["official-artwork"].front_default} className="poke-img"/>
             <h4>{pokemon.name}</h4>
-            <div className="row-container">
+            <div className="type-container">
                 {pokemon.types.map((t,i) => <h4 className={`type ${t.type.name}`}>{t.type.name}</h4>)}
             </div>
-            {pokemon.stats.map((s, i) => <h5>{s.stat.name} {s.base_stat}</h5> )}
+
+            <div className="stat-container">
+            {pokemon.stats.map((s, i) => <h5 className="stat">{s.stat.name} {s.base_stat}</h5> )}
+            </div>
+            <button>Remove</button>
         </div>
     )
 }
